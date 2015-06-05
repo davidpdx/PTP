766186729dfa4e1fd144534a00cfa5cbf6f81d12
DD <realemailaddress999@gmail.com>
2015-06-05 09:31:10 -0700

    added additional pages

---
+++ scripts/scripts-done.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('a[rel="ireland"]').colorbox({
+    width: '60%',
+    height: '70%',
+    slideshow: true,
+    slideshowSpeed: 2000,
+    slideshowAuto: false,
+    slideshowStart: "Start Slideshow",
+    slideshowStop: "Stop Slideshow"  });
+});
